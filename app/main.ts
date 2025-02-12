dcc72ddb0946b5a2c10b8025d27bcd4e3c4470af
Yuri FA <yurik45gb@gmail.com>
2025-02-12 23:42:17 +0300

    codecrafters submit [skip ci]

--- app/main.ts
+++ app/main.ts
@@ -5,16 +5,47 @@ const rl = createInterface({
   output: process.stdout,
 });
 
+const COMMANDS = {
+  exit: {
+    command: "exit",
+    description: "is a shell builtin",
+  },
+  echo: {
+    command: "echo",
+    description: "is a shell builtin",
+  },
+  type: {
+    command: "type",
+    description: "is a shell builtin",
+  },
+} as const;
+
+const COMMANDS_LIST = Object.values(COMMANDS).map((command) => command.command);
+
 function repl() {
   rl.question("$ ", (answer) => {
     const [command, ...args] = answer.split(" ");
     switch (command) {
-      case "exit": {
+      case COMMANDS.exit.command:
         return rl.close();
-      }
 
-      case "echo":
-        rl.write(`${args.join(' ')}\n`);
+      case COMMANDS.echo.command:
+        rl.write(`${args.join(" ")}\n`);
+        break;
+
+      case COMMANDS.type.command:
+        let targetCommand = args[0];
+        if (
+          COMMANDS_LIST.includes(
+            targetCommand as (typeof COMMANDS_LIST)[number],
+          )
+        ) {
+          rl.write(
+            `${args[0]} ${COMMANDS[targetCommand as keyof typeof COMMANDS].description}\n`,
+          );
+        } else {
+          rl.write(`${args[0]}: not found\n`);
+        }
         break;
 
       default:
