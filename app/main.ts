522bcf17fc2dd9a67b6069eca2c10ab074405d84
Yuri FA <yurik45gb@gmail.com>
2025-02-16 15:30:07 +0300

    fix: add redirecting to stdout

--- app/main.ts
+++ app/main.ts
@@ -13,99 +13,148 @@ const rl = createInterface({
 const PATH_ENV = process.env.PATH;
 const HOME_ENV = process.env.HOME;
 
-const COMMANDS_LIST = ["echo", "exit", "pwd", "type"];
+const builtinCommands = new Map<
+  string,
+  { execute: (args: string[]) => void }
+>();
+builtinCommands.set("exit", {
+  execute: () => rl.close(),
+});
+builtinCommands.set("echo", {
+  execute: (args) => rl.write(`${args.join(" ")}\n`),
+});
+builtinCommands.set("pwd", {
+  execute: () => rl.write(`${cwd()}\n`),
+});
+
+builtinCommands.set("cd", {
+  execute: (args) => {
+    const path = args[0];
+    let resultPath = path;
+
+    if (path.startsWith("~")) {
+      resultPath = nodePath.resolve(
+        HOME_ENV || "",
+        path.slice(1),
+        ...args.slice(1),
+      );
+    }
+
+    if (path.startsWith(".")) {
+      resultPath = nodePath.resolve(cwd(), ...args);
+    }
+
+    if (fs.existsSync(resultPath)) {
+      chdir(resultPath);
+    } else {
+      rl.write(`cd: ${resultPath}: No such file or directory\n`);
+    }
+  },
+});
+
+builtinCommands.set("type", {
+  execute: (args) => {
+    const targetCommand = args[0];
+    if (builtinCommands.has(targetCommand)) {
+      rl.write(`${targetCommand} is a shell builtin\n`);
+      return;
+    }
+
+    const paths = PATH_ENV?.split(":") || [];
+
+    let finded = paths.find((path) => {
+      return fs.existsSync(nodePath.resolve(path, targetCommand));
+    });
+
+    if (!finded) {
+      rl.write(`${targetCommand}: not found\n`);
+      return;
+    }
+    rl.write(`${targetCommand} is ${finded}/${targetCommand}\n`);
+  },
+});
+
+const ensureDirectoriesExist = (filePath: string) => {
+  const dirPath = nodePath.dirname(filePath);
+
+  // Split the directory path into its components
+  const pathSegments = dirPath.split(nodePath.sep);
+
+  // Reconstruct the path step by step and check each segment
+  let currentPath = pathSegments[0] === "" ? nodePath.sep : pathSegments[0];
+  for (let i = 1; i < pathSegments.length; i++) {
+    currentPath = nodePath.join(currentPath, pathSegments[i]);
+
+    // Check if the current path exists
+    if (!fs.existsSync(currentPath)) {
+      throw new Error(`Directory does not exist: ${currentPath}`);
+    }
+  }
+};
+
+const executeNonBuiltinCommand = (
+  command: string,
+  args: string[],
+  originalQuery: string,
+) => {
+  const paths = PATH_ENV?.split(":") || [];
+
+  const finded = paths.find((path) => {
+    return fs.existsSync(nodePath.resolve(path, command));
+  });
+  // console.log({ command, args, finded });
+
+  if (!finded) {
+    rl.write(`${originalQuery}: command not found\n`);
+    return;
+  }
+
+  const redirectArgIndex = args.findIndex((arg) => ["1>", ">"].includes(arg));
+
+  if (redirectArgIndex && args.length > redirectArgIndex) {
+    const outputFileName = args[redirectArgIndex + 1];
+
+    // try {
+    //   ensureDirectoriesExist(outputFileName);
+    // } catch (error) {
+    //   console.log({ error });
+    // }
+    try {
+      const outputFile = fs.openSync(outputFileName, "w");
+      console.log({ redirectArgIndex, outputFileName });
+      const result = spawnSync(command, args.slice(0, redirectArgIndex), {
+        stdio: ["inherit", outputFile, "inherit"], // Redirect stdout to the file
+      });
+      fs.closeSync(outputFile);
+    } catch (error) {
+      rl.write(`no such file or directory: ${outputFileName}\n`);
+    }
+  } else {
+    const result = spawnSync(command, args, {
+      stdio: "inherit",
+    });
+  }
+
+  // if (result.error) {
+  //   console.error(`Error: ${result.error.message}`);
+  // } else {
+  //   console.log(`Output:\n${result.stdout}`);
+  //   if (result.stderr) console.error(`stderr: ${result.stderr}`);
+  // }
+
+  // rl.write(`${result.toString()}`);
+};
 
 function repl() {
   rl.question("$ ", (answer) => {
     let [command, ...args] = splitArgs(answer);
 
-    switch (command) {
-      case "exit": {
-        return rl.close();
-      }
+    const commandBuiltin = builtinCommands.get(command);
 
-      case "echo": {
-        rl.write(`${args.join(" ")}\n`);
-        break;
-      }
-
-      case "pwd": {
-        rl.write(`${cwd()}\n`);
-        break;
-      }
-
-      case "cd": {
-        const path = args[0];
-        let resultPath = path;
-
-        if (path.startsWith("~")) {
-          resultPath = nodePath.resolve(
-            HOME_ENV || "",
-            path.slice(1),
-            ...args.slice(1),
-          );
-        }
-
-        if (path.startsWith(".")) {
-          resultPath = nodePath.resolve(cwd(), ...args);
-        }
-
-        if (fs.existsSync(resultPath)) {
-          chdir(resultPath);
-        } else {
-          rl.write(`${command}: ${resultPath}: No such file or directory\n`);
-        }
-        break;
-      }
-
-      case "type": {
-        const targetCommand = args[0];
-        if (COMMANDS_LIST.includes(targetCommand)) {
-          rl.write(`${targetCommand} is a shell builtin\n`);
-          break;
-        }
-
-        const paths = PATH_ENV?.split(":") || [];
-
-        let finded = paths.find((path) => {
-          return fs.existsSync(nodePath.resolve(path, targetCommand));
-        });
-
-        if (finded) {
-          rl.write(`${targetCommand} is ${finded}/${targetCommand}\n`);
-          break;
-        }
-
-        rl.write(`${targetCommand}: not found\n`);
-        break;
-      }
-
-      default: {
-        const paths = PATH_ENV?.split(":") || [];
-
-        const finded = paths.find((path) => {
-          return fs.existsSync(nodePath.resolve(path, command));
-        });
-
-        if (finded) {
-          const result = spawnSync(command, args, {
-            stdio: "inherit",
-          });
-
-          // const result = spawnSync(`${finded}/${command}`, args);
-          // if (result.error) {
-          //   console.error(`Error: ${result.error.message}`);
-          // } else {
-          //   console.log(`Output:\n${result.stdout}`);
-          //   if (result.stderr) console.error(`stderr: ${result.stderr}`);
-          // }
-
-          // rl.write(`${result.toString()}`);
-          break;
-        }
-
-        rl.write(`${answer}: command not found\n`);
-      }
+    if (commandBuiltin) {
+      commandBuiltin.execute(args);
+    } else {
+      executeNonBuiltinCommand(command, args, answer);
     }
 
     repl();
